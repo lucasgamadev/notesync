053003d60fb57b8c5eb7f3981872477fc387c655
lucasgamadev <lucas.da.gama.lima@gmail.com>
2025-05-05 00:30:25 -0300

    Adiciono configuração PWA para o frontend
    Implemento a configuração inicial para Progressive Web App (PWA) no frontend usando `next-pwa`.
    
    Defino estratégias de cache em tempo de execução para diversos tipos de assets, como fontes do Google, fontes locais, imagens, scripts, estilos, dados e chamadas de API, visando melhorar o desempenho e habilitar o suporte offline.
    
    A funcionalidade PWA está desabilitada em ambiente de desenvolvimento.

---
+++ frontend/next-pwa.config.js
@@ -0,0 +1,101 @@
+/** @type {import('next-pwa').PWAConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "google-fonts",
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60 // 1 ano
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-font-assets",
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 7 * 24 * 60 * 60 // 7 dias
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-image-assets",
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 30 * 24 * 60 * 60 // 30 dias
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:js)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-js-assets",
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60 // 24 horas
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:css|less)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-style-assets",
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60 // 24 horas
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:json|xml|csv)$/i,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "static-data-assets",
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60 // 24 horas
+        }
+      }
+    },
+    {
+      urlPattern: /\/api\/.*$/i,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "apis",
+        expiration: {
+          maxEntries: 16,
+          maxAgeSeconds: 24 * 60 * 60 // 24 horas
+        },
+        networkTimeoutSeconds: 10 // timeout após 10 segundos
+      }
+    },
+    {
+      urlPattern: /.*/i,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "others",
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60 // 24 horas
+        },
+        networkTimeoutSeconds: 10
+      }
+    }
+  ]
+});
+
+module.exports = withPWA;
